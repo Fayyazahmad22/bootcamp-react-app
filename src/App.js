99d3d1ca4d1bec114b83962e25607c18f6ae6b33
malikfayyaz22 <malikfayyaz22@gmail.com>
2020-11-09 00:38:35 +0930

    Added react component
    learnt about components, DOM, React introduction and created a component in react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import React from 'react';
+import Dinner from './dinner.js';
+
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>Hello World! From Fayyaz ahmad</h1>
+      <hr/>
+
+      <Dinner dishName="Chicken Karahi" sweetDish="kheer"/>
+      <hr/>
+      <Dinner dishName="Biryani" sweetDish="jaleebi"/>
+      <hr/>
+      <Dinner dishName="Nihaari" sweetDish="Halwa"/>
     </div>
   );
 }
